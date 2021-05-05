e0c63d3b145f8fedcec52b3502688ce6f02126ca
Umberto Valleriani <umberto.valleriani@gmail.com>
2021-05-05 18:42:27 +0200

    Unfuck the main flow of the script

--- doit.js
+++ doit.js
@@ -32,6 +32,11 @@ const bakeryRouter = new web3.eth.Contract(
     addresses.bakery.router
 );
 
+const flashswap = new web3.eth.Contract(
+    Flashswap.abi,
+    //Flashswap.networks[networkId].address
+    '0X4C2C14C41400DEE551EF0F45F32508CAB2A129DD'
+);
 
 class Token {
     constructor(name, address, decimals, startAmt) {
@@ -66,7 +71,7 @@ class Pair {
 }
 
 
-const WBNB = new Token('WBNB', '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 80);
+const WBNB = new Token('WBNB', '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 10);
 const BUSD = new Token('BUSD', '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', 18, 10000);
 const USDT = new Token('USDT', '0x55d398326f99059ff775485246999027b3197955', 18, 10000);
 const BAKE = new Token('BAKE', '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5', 18, 1000);
@@ -86,83 +91,12 @@ const init = async () => {
     const networkId = await web3.eth.net.getId();
 
     console.log('Getting flashswap contract abi');
-    const flashswap = new web3.eth.Contract(
-        Flashswap.abi,
-        //Flashswap.networks[networkId].address
-        '0X4C2C14C41400DEE551EF0F45F32508CAB2A129DD'
-    );
 
     for (const pair of pairs) {
-        debugger;
-        const tokenA = pair.token0;
-        const tokenB = pair.token1;
-        const amount = tokenA.startAmt;
 
-        if (tokenA.name == tokenB.name) {
-            continue;
-        }
-        console.log(`Trading ${tokenB.name}/${tokenA.name} ...`);
+        const amount = pair.token0.startAmt;
+        await tryPerformingArbitrage(pair, amount);
 
-
-        const pairAddress = await getPairAddressFrom(pancakeFactory, pair);
-
-        // const pairAddress = await pancakeFactory.methods.getPair(tokenA.address, tokenB.address).call();
-        // console.log(`pairAddress ${tokenB.name}/${tokenA.name} is ${pairAddress}`);
-
-
-        const unit0 = await new BigNumber(amount);
-        let amount0 = await new BigNumber(unit0).shiftedBy(tokenA.decimals);
-        // let amount0 = await new BigNumber(unit0).shiftedBy(tokenA.decimals);
-        console.log(`Input amount of ${tokenA.name}: ${unit0.toString()}`);
-
-
-        tokenIn = tokenA.address;
-        tokenInName = tokenA.name;
-        tokenOut = tokenB.address;
-        tokenOutName = tokenB.name;
-
-
-        // Calcolo quante banane mi danno con amount0 di $tokenIn su exchangeX
-        //    [value, wei]
-        const [unit1, amount1] = await calculateSwapOn(
-                                                pancakeRouter, amount0, [tokenIn, tokenOut]);
-        console.log(`
-            Buying ${pair.name0} at PancakeSwap DEX
-            =================
-            Send: ${unit0.toString()} ${tokenInName}
-            Receive: ${unit1.toString()} ${tokenOutName}
-        `);
-
-        // Calcolo quanti $tokenIn mi danno con amount1 di banane su exchangeY
-        //    [value, wei]
-        const [unit2, amount2] = await calculateSwapOn(
-                                                bakeryRouter, amount1, [tokenOut, tokenIn]);
-        console.log(`
-            Selling ${pair.name0} at BakerySwap DEX
-            =================
-            Send: ${unit1.toString()} ${tokenOutName}
-            Receive: ${unit2.toString()} ${tokenInName}
-        `);
-
-        let profit = await new BigNumber(amount2).minus(amount0).shiftedBy(-tokenA.decimals);
-        console.log(`Profit: ${profit.toString()} ${tokenInName}`);
-
-        if (profit > 0) {
-            await startArbitrage(
-                tokenA,
-                tokenB,
-                0,
-                amount1
-            );
-        } else {
-            console.log(`
-                Arbitrage opportunity not found!
-                Expected profit: ${profit}
-                amount0: ${amount0 / 10**18} ${pair.name0}
-                amount1: ${amount1 / 10**18} ${pair.name1}
-                amount2: ${amount2 / 10**18} ${pair.name0}
-            `);
-        }
     }
 }
 
@@ -193,6 +127,69 @@ async function calculateSwapOn(router, amount, path) {
     const value = await new BigNumber(amounts[1]).shiftedBy(-18);
     const wei = await new BigNumber(amounts[1]);
     return [ value, wei ]
+}
+
+async function tryPerformingArbitrage(pair, amount) {
+
+    const tokenA = pair.token0;
+    const tokenB = pair.token1;
+    console.log(`Trading ${tokenB.name}/${tokenA.name} ...`);
+
+    const pairAddress = await getPairAddressFrom(pancakeFactory, pair);
+
+
+    const unit0 = await new BigNumber(amount);
+    let amount0 = await fromWei(amount);
+    console.log(`Input amount of ${tokenA.name}: ${unit0.toString()}`);
+
+
+    tokenIn = tokenA.address;
+    tokenInName = tokenA.name;
+    tokenOut = tokenB.address;
+    tokenOutName = tokenB.name;
+
+
+    // Calcolo quante banane mi danno con amount0 di $tokenIn su exchangeX
+    //    [value, wei]
+    const [unit1, amount1] = await calculateSwapOn(
+                                            pancakeRouter, amount0, [tokenIn, tokenOut]);
+    console.log(`
+        Buying ${pair.name0} at PancakeSwap DEX
+        =================
+        Send: ${unit0.toString()} ${tokenInName}
+        Receive: ${unit1.toString()} ${tokenOutName}
+    `);
+
+    // Calcolo quanti $tokenIn mi danno con amount1 di banane su exchangeY
+    //    [value, wei]
+    const [unit2, amount2] = await calculateSwapOn(
+                                            bakeryRouter, amount1, [tokenOut, tokenIn]);
+    console.log(`
+        Selling ${pair.name0} at BakerySwap DEX
+        =================
+        Send: ${unit1.toString()} ${tokenOutName}
+        Receive: ${unit2.toString()} ${tokenInName}
+    `);
+    let profit = await new BigNumber(amount2).minus(amount0).shiftedBy(-tokenA.decimals);
+    console.log(`Profit: ${profit.toString()} ${tokenInName}`);
+
+    if (profit > 0) {
+        await startArbitrage(
+            tokenA,
+            tokenB,
+            0,
+            amount1
+        );
+    } else {
+        console.log(`
+            Arbitrage opportunity not found!
+            Expected profit: ${profit} ${pair.name0}
+            amount0: ${amount0 / 10**18} ${pair.name0}
+            amount1: ${amount1 / 10**18} ${pair.name1}
+            amount2: ${amount2 / 10**18} ${pair.name0}
+        `);
+    }
+
 }
 
 /*
